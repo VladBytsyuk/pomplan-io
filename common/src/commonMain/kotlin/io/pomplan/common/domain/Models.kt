3a38de5e87dffe3fabc19494ba153b19ca8ade66
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-02-13 14:47:12 +0300

    Implemented State extensions

--- common/src/commonMain/kotlin/io/pomplan/common/domain/Models.kt
+++ common/src/commonMain/kotlin/io/pomplan/common/domain/Models.kt
@@ -9,4 +9,12 @@ data class Timer(
     enum class Mode { PRE_WORK, WORK, PRE_BREAK, BREAK }
 }
 
-data class Time(val milliseconds: Long)
+data class Time(val milliseconds: Long = 0) {
+    init { require(milliseconds >= 0) }
+
+    constructor(minute: Int = 0, second: Int = 0) :
+            this(milliseconds = minute * MILLIS_IN_MINUTE + second * MILLIS_IN_SECOND)
+
+    val minute: Int get() = (milliseconds / MILLIS_IN_MINUTE).toInt()
+    val seconds: Int get() = (milliseconds % MILLIS_IN_MINUTE / MILLIS_IN_SECOND).toInt()
+}
--- common/src/commonMain/kotlin/io/pomplan/common/elm/StateExtensions.kt
+++ common/src/commonMain/kotlin/io/pomplan/common/elm/StateExtensions.kt
@@ -1,2 +1,33 @@
 package io.pomplan.common.elm
 
+import io.pomplan.common.domain.Time
+import io.pomplan.common.domain.Timer
+import io.pomplan.common.domain.second
+
+
+fun State.changeTimerMode(mode: Timer.Mode): State = copy(
+    timer = timer.copy(mode = mode)
+)
+
+fun State.changeGoalTime(time: Time): State = copy(
+    timer = timer.copy(goalTime = time)
+)
+
+fun State.changeElapsedTime(time: Time): State = copy(
+    timer = timer.copy(elapsedTime = time)
+)
+
+fun State.addSecondToElapsedTime(): State = copy(
+    timer = timer.copy(
+        elapsedTime = Time(milliseconds = timer.elapsedTime.milliseconds + 1.second)
+    )
+)
+
+fun State.takeSecondFromElapsedTime(): State = copy(
+    timer = timer.copy(
+        elapsedTime = when {
+            timer.elapsedTime.milliseconds < 1.second -> Time(minute = 0, second = 0)
+            else -> Time(milliseconds = timer.elapsedTime.milliseconds - 1.second)
+        }
+    )
+)
