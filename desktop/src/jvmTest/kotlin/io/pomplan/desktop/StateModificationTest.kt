03c795e0fd043afa9d454fdbc8554648b0b67d0a
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-02-16 22:55:06 +0300

    Added tests for state extensions

--- desktop/src/jvmTest/kotlin/io/pomplan/desktop/StateModificationTest.kt
+++ desktop/src/jvmTest/kotlin/io/pomplan/desktop/StateModificationTest.kt
@@ -6,17 +6,14 @@ import io.pomplan.common.domain.Pomodoro
 import io.pomplan.common.domain.Pomodoro.Mode.*
 import io.pomplan.common.domain.Time
 import io.pomplan.common.elm.*
-import io.pomplan.common.kodein
-import org.kodein.di.instance
 
 
 class StateModificationTest : ShouldSpec({
-    should("2 + 2 = 4") {
-        (2 + 2) shouldBe 4
-    }
-
     context(name = "Initial state") {
         val initialState = State.initial
+        should("be in the same state after pause") { initialState.pausePomodoro() shouldBe initialState }
+        should("be in the same state after stop") { initialState.stopPomodoro() shouldBe initialState }
+        should("be in the same state after tick") { initialState.tick() shouldBe initialState }
         should("be in work mode after play") {
             initialState.playPomodoro() shouldBe State(
                 pomodoro = Pomodoro(
@@ -26,12 +23,6 @@ class StateModificationTest : ShouldSpec({
                     lastDoneNumber = 0
                 )
             )
-        }
-        should("be in the same state after pause") {
-            initialState.pausePomodoro() shouldBe initialState
-        }
-        should("be in the same state after stop") {
-            initialState.stopPomodoro() shouldBe initialState
         }
         should("be in pre break mode with 1 done pomodoro after skip") {
             initialState.skipPomodoro() shouldBe State(
@@ -43,8 +34,56 @@ class StateModificationTest : ShouldSpec({
                 )
             )
         }
-        should("be in the same state after tick") {
-            initialState.tick() shouldBe initialState
+    }
+    context(name = "Work state") {
+        val workState = State(
+            pomodoro = Pomodoro(
+                mode = WORK,
+                goalTime = Time(minute = 25),
+                elapsedTime = Time(minute = 24, second = 15),
+                lastDoneNumber = 0
+            )
+        )
+        should("be in the pre work state after pause") {
+            workState.pausePomodoro() shouldBe State(
+                pomodoro = Pomodoro(
+                    mode = PRE_WORK,
+                    goalTime = Time(minute = 25),
+                    elapsedTime = Time(minute = 24, second = 15),
+                    lastDoneNumber = 0
+                )
+            )
+        }
+        should("be in the pre work state after stop") {
+            workState.stopPomodoro() shouldBe State(
+                pomodoro = Pomodoro(
+                    mode = PRE_WORK,
+                    goalTime = Time(minute = 25),
+                    elapsedTime = Time(minute = 25),
+                    lastDoneNumber = 0
+                )
+            )
+        }
+        should("be in the work state with decreased elapsed time after tick") {
+            workState.tick() shouldBe State(
+                pomodoro = Pomodoro(
+                    mode = WORK,
+                    goalTime = Time(minute = 25),
+                    elapsedTime = Time(minute = 24, second = 14),
+                    lastDoneNumber = 0
+                )
+            )
+        }
+        should("be in the same mode after play") { workState.playPomodoro() shouldBe workState }
+        should("be  in pre break mode with 1 done pomodoro after skip") {
+            workState.skipPomodoro() shouldBe State(
+                pomodoro = Pomodoro(
+                    mode = PRE_BREAK,
+                    goalTime = Time(minute = 5),
+                    elapsedTime = Time(minute = 5),
+                    lastDoneNumber = 1
+                )
+            )
         }
     }
 })
