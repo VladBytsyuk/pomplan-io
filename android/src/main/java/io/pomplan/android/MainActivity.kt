d1a70f30a1d0c29274cc0644c47fd2a2d9f470a0
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2021-03-25 20:57:25 +0300

    Implemented ViewBinding

--- android/src/main/java/io/pomplan/android/MainActivity.kt
+++ android/src/main/java/io/pomplan/android/MainActivity.kt
@@ -1,11 +1,9 @@
 package io.pomplan.android
 
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import io.pomplan.android.databinding.ActivityMainBinding
 import io.pomplan.android.ui_kit.PomodoroPanelView
-import io.pomplan.android.ui_kit.TimerProgressView
 import io.pomplan.android.ui_kit.TimerView
 import io.pomplan.common.domain.Pomodoro.Mode.*
 import io.pomplan.common.elm.Action
@@ -17,9 +15,12 @@ import io.pomplan.common.inject
 class MainActivity : AppCompatActivity() {
     private val controller: Controller by inject()
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setOnClickListeners()
     }
 
@@ -36,15 +37,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOnClickListeners() {
-        findViewById<ImageView>(R.id.btnPlayPause).setOnClickListener {
+        binding.btnPlayPause.setOnClickListener {
             val action = if (controller.currentState.pomodoro.mode in listOf(PRE_WORK, PRE_BREAK))
                 Action.UserClick.Button.Play else Action.UserClick.Button.Pause
             controller.setAction(action)
         }
-        findViewById<ImageView>(R.id.btnStop).setOnClickListener {
+        binding.btnStop.setOnClickListener {
             controller.setAction(Action.UserClick.Button.Stop)
         }
-        findViewById<ImageView>(R.id.btnSkip).setOnClickListener {
+        binding.btnSkip.setOnClickListener {
             controller.setAction(Action.UserClick.Button.Skip())
         }
     }
@@ -52,15 +53,15 @@ class MainActivity : AppCompatActivity() {
     private fun render(data: State) {
         val pomodoro = data.pomodoro
 
-        findViewById<TimerView>(R.id.timer).data = TimerView.Data(
+        binding.timer.data = TimerView.Data(
             goal = pomodoro.goalTime,
             elapsed = pomodoro.elapsedTime,
             mode = pomodoro.mode,
         )
-        findViewById<ImageView>(R.id.btnPlayPause).setImageResource(
+        binding.btnPlayPause.setImageResource(
             if (pomodoro.mode in listOf(PRE_WORK, PRE_BREAK)) R.drawable.ic_play else R.drawable.ic_pause
         )
-        findViewById<PomodoroPanelView>(R.id.pomodoroPanel).count = PomodoroPanelView.Data(
+        binding.pomodoroPanel.count = PomodoroPanelView.Data(
             doneCount = pomodoro.number,
             currentInProgress = pomodoro.mode in listOf(WORK, PRE_WORK)
         )
